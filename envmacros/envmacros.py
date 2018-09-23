e759a94e332b105660705055946ba8b99a017a78
Duane Ellis <duane@duaneellis.com>
2018-09-22 22:04:47 -0700

    Add support for expressions

--- envmacros/envmacros.py
+++ envmacros/envmacros.py
@@ -2,8 +2,25 @@
 import re
 import os
 import time
+import math
+import sys
+from frozenclass import FrozenClass
 
-__all__ = ['MacroError','MacroDictionary','MacroResolver','MacroResult']
+# We don't specifically use 'math'
+# but by importing this you can use
+# things like math.cos() and sin() in expressions.
+
+from math import *
+
+__all__ = ['MacroError',
+           'MacroLookup',
+           'MacroResolver',
+           'MacroResult',
+           'ExpressionEvaluator',
+           'lookup',
+           'resolver',
+           'evaluator'
+]
 
 
 # this regex matches a C (or bash) style symbol, in the form: ${NAME}
@@ -11,16 +28,23 @@ __all__ = ['MacroError','MacroDictionary','MacroResolver','MacroResult']
 #    "before" - stuff before the invocation.
 #    "name" - the name of the macro
 #    "after" - the text after the macro
-c_symbol = re.compile( "^(?P<before>.*)\$\{(?P<name>[A-Za-z_][0-9A-Za-z_]*)\}(?P<after>.*)$" )
+_re_symbol = re.compile( "^(?P<before>.*)\$\{(?P<name>[A-Za-z_][0-9A-Za-z_]*)\}(?P<after>.*)$" )
 
 DEBUG=False
 
 class MacroError( Exception ):
     pass
 
-class MacroDictionary( object ):
+if sys.version_info[0] == 2:
+    _str_able_types = (int, float,long )
+else:
+    _str_able_types = (int, float)
+
+
+@FrozenClass
+class MacroLookup( object ):
     """
-    This represents a basic dictionary like object
+    This represents a basic macro lookup class
     that holds your macro variable names.
     """
     def __init__(self, allow_env = True):
@@ -30,42 +54,76 @@ class MacroDictionary( object ):
 
     def set_parent( self, parent ):
         self.parent = parent
-        
-    def add( self, name, value ):
+
+    def add( self, name, value, where = None ):
         """
         Add a name value pair.
+        With an optional location (ie: where defined)
         """
-        self.entries[name] = value
+        
+        if isinstance( value, _str_able_types ):
+            value = str(value)
+        assert( isinstance( value, str ) )
+        self.entries[name] = (value, where)
 
     def lookup( self, result, name ):
         """
         Return the text (or none) for macro 'name'.
         """
-        value = self.entries.get( name, None )
-        if value != None:
-            return value
-        if self.allow_env:
+
+        value = None
+        where = None
+        
+        # is it in our dictonary?
+        tuple_ = self.entries.get( name, None )
+        if tuple_:
+            value = tuple_[0]
+            where = tuple_[1]
+
+        # if not found
+        if (value == None) and self.allow_env:
             value = os.environ.get( name, None )
             if value != None:
-                return value
-        # dynamic?
-        func = getattr( self, 'macro_' + name, None)
-        if func is None:
-            return None
-        return func( name )
+                where = "os.environ[%s]" % name
 
-    def macro_NOW(self):
+        # is it dynamic?
+        if value == None:
+            fname =  'macro_' + name
+            func = getattr( self, fname, None)
+            if func != None:
+                where = "function: %s()" % fname
+                try:
+                    value = func( name, result )
+                except Exception as e:
+                    result.err_msg = "Exception: %s() -> %s" % (fname, str(e))
+                    return None
+
+        if value == None:
+            result.err_msg = "Undefined: %s" % name
+            return None
+        
+        if where != None:
+            result.add_step("%s: %s -> %s" % (where, name, value))
+        else:
+            result.add_step("%s -> %s" % (name,value))
+        return value
+
+    def macro_NOW(self, name, result):
         """
         This handles the macro ${NOW}
         """
         return time.ctime()
 
-    def macro_GETCWD(self):
+    def macro_GETCWD(self, name, result):
         """
         This handles the macro ${GETCWD}
         """
         return os.getcwd()
 
+# our global "macros"
+lookup = MacroLookup()
+    
+@FrozenClass
 class MacroResult(object):
     """
     A macro resolution result.
@@ -74,66 +132,255 @@ class MacroResult(object):
     If self.result is None, then see self.err_msg.
     Additional debug information can be found in self.steps
     """
+    PASS_MAX = 50
     def __init__(self):
+        self.pass_count = 0
+        self.pass_max   = self.PASS_MAX
         self.err_msg = None
-        self.done = False
         self.result = None
         self.steps = []
 
     def add_step( self, txt ):
         self.steps.append( txt )
 
+@FrozenClass
 class MacroResolver(object):
-    def __init__(self, lookup = None ):
+    """
+    Evaluate a string, replacing all ${macros} with their values.
+    Returns a MacroResult()
+    """
+    def __init__(self, my_lookup = None ):
         self.result = None
-        self.pass_max   = 50
-        self.pass_count = 0
-        if lookup == None:
-            lookup = MacroDictionary()
-        self.lookup = lookup
+        if my_lookup == None:
+            global lookup
+            my_lookup = lookup
+        assert( isinstance( my_lookup, MacroLookup ) )
+        self.lookup = my_lookup
         self.lookup.set_parent( self )
 
-    def resolve( self, text ):
+    def resolve( self, text, result=None ):
         """
         Given text, resolve all macros.
         """
-        self.pass_count = 0
-        self.result = MacroResult()
-        self.result.add_step("start: %s" % text )
-        self.result.result = text
-        while self._make_pass():
-            self.pass_count += 1
-            if self.pass_count > self.pass_max:
-                self.result.add_step("too many passes")
-                self.result.err_msg = "Too many passes"
-                break
-            self.result.add_step("pass: %d -> %s" % (self.pass_count, self.result.result))
-        r = self.result
-        self.result = None
-        # if an error occurred
-        if r.err_msg != None:
-            # do not return text.
-            r.result = None
-        return r
-
-    def _make_pass(self):
-        # Make a single pass over the string resolving macros.
-        m = c_symbol.match( self.result.result )
-        if m is None:
-            # no invocation found
-            # FUTURE: support function like macros
-            # for example:  ${str.upper(${foo})}
-            return False
-        name = m.group('name')
-        result = self.lookup.lookup( self.result, name )
         if result == None:
-            self.result.err_msg = "Undefined: %s" % name
+            result = MacroResult()
+        result.add_step("start: %s" % text )
+        result.err_msg = None
+        result.result  = text
+
+        # ALL macros have $ signs.
+        if '$' not in result.result:
+            # if no $ signs occur.. we are done
+            return result
+        
+        while self._make_pass(result):
+            result.pass_count += 1
+            if result.pass_count > result.pass_max:
+                result.add_step("too many passes")
+                result.err_msg = "Too many passes"
+                break
+            result.add_step("pass: %d -> %s" % (result.pass_count, result.result))
+        # if an error occurred
+        if result.err_msg != None:
+            # do not return text.
+            result.result = None
+        return result
+
+    def _make_pass(self, result):
+        # FUTURE: support function like macros
+        # for example:  ${str.upper(${foo})}
+        
+        # Make a single pass over the string resolving macros.
+        m = _re_symbol.match( result.result )
+        if m is None:
+            # no macro invocation found
             return False
-        self.result.result = m.group('before') + result + m.group('after')
+
+        mname = m.group('name')
         if DEBUG:
-            self.result.add_step("${%s}->%s" % (name, result))
-            self.result.add_step("before='%s'" % m.group('before'))
-            self.result.add_step("after='%s'" % m.group('after'))
-            self.result.add_step("New: %s" % self.result.result )
+            result.add_step("text-before='%s'" % m.group('before'))
+            result.add_step("macro: %s" % mname )
+            result.add_step("text-after='%s'" % m.group('after'))
+        
+        txt = self.lookup.lookup( result, mname )
+        if txt == None:
+            return False
+        result.result = m.group('before') + txt + m.group('after')
+        result.add_step("New: %s" % result.result )
         return True
+
+resolver = MacroResolver(lookup)
+
+# match hex numbers
+_re_hex = re.compile('0[xX][0-9a-fA-F]+')
+
+# what strings are ok in our restricted number only modemode?
+_ok_whitelist = set()
+_ok_whitelist.add('True')
+_ok_whitelist.add('False')
+_ok_whitelist.add('and')
+_ok_whitelist.add('or')
+_ok_whitelist.add('not')
+# All functions in math
+for tmp in dir(math):
+    if tmp[0] != '_':
+        tmp = tmp + '('
+        _ok_whitelist.add(tmp)
+
+# digits or numbers
+_re_digits  = re.compile(  r'^[0123456789.]+$')
+
+# these are chars we can ignore
+# or safely transform to spaces
+_str_from = "-+*/&|%!~<>=.)"
+_str_to   = "              "
+
+try:
+    # Python > 3.1
+    _table_to_space = str.maketrans( _str_from, _str_to )
+except:
+    # Python < 3.1
+    import string
+    _table_to_space = string.maketrans( _str_from, _str_to )
+
+def _safe_eval_check( text ):
+    ''' 
+    Returns None if the string is ok to eval.
+
+    Returns an error message if the string is not safe to eval.
+
+    The technique is generally replace safe things with spaces
+    Then split the string on spaces and deal with each possbile
+    thing we might find - we are not checking syntax here.
+    We let python handle the syntax.
+    '''
+    
+    # Python allows spaces between the function
+    # and the opening parens we need to fix that.
+    # for example:  "cos   (123)" we must
+    # convert this into "cos(123)"
+
+    oldlen = len(text)
+    while True:
+        # replace what we can
+        text = text.replace( ' (', '(' )
+        # If any change occurs, our string is shorter
+        newlen = len(text)
+        if oldlen == newlen:
+            # no change, we are done
+            break
+        # Try again
+        oldlen = newlen
+
+
+    # Now, we might have a string like this:
+    # For example:   "cos(123)*(4 >= ~123) && False < 7"
+    #
+    # What matters to us is is the "cos(" part that could
+    # instead be some evilfunction() call.
+
+    # We need to seperate cos(123) into: "cos( 123 )"
+    # by adding spaces we can use str.split() to parse.
+    #
+    text = text.replace( '(', '( ')
+
+    # our string is now something like this:
+    # And we can throw away all of our math operators.
+    #
+    # old: "cos(123)*(4 >= ~123) && False < 7"
+    # new: "cos( 123   4     123     False   7"
+    text = text.translate( _table_to_space )
+
+    # we have a white list of function names.
+    # we can check each part of the new string
+    # against our white list of function names.
+    for part in text.split( ' ' ):
+
+        # ignore blanks
+        if len(part)==0:
+            continue
+
+        if part == '(':
+            # this happens in simple expressions
+            continue
+        
+        # Most common will be pure digits.
+        m = _re_digits.match( part )
+        if m != None:
+            continue
+
+        # Allow hex digits
+        m = _re_hex.match( part )
+        if m != None:
+            continue
+
+        # We have something..
+        # It could be: "True" or "False"
+        # It could be a function call, like: "cos("
+        # or the start some "evil(" function call.
+        #
+        # Check it against our white list of function calls.
+        if part in _ok_whitelist:
+            # good
+            continue
+        
+        # Not recognized
+        return "Illegal: %s" % part
+    # nothing bad found
+    return None
+
+@FrozenClass
+class ExpressionEvaluator( object ):
+    '''
+    Given a string, which may contain macros
+    and should contain an expression of some sort..
+    
+    Evaluate the expression and return the result.
+    '''
+
+    def __init__(self, macro_resolver = None):
+        if macro_resolver == None:
+            # it is our default value
+            global resolver
+            macro_resolver = resolver
+        assert( isinstance( resolver, MacroResolver ) )
+        self.resolver = macro_resolver
+        
+    def eval( self, text, result = None ):
+        
+        result = self.resolver.resolve( text, result )
+        if result.err_msg != None:
+            result.result = None
+            return result
+
+        if result.result != text:
+            result.add_step("Evalutate: '%s'" % result.result)
+
+        result.result = result.result.strip()
+        if len( result.result ) == 0:
+            result.err_msg = "Empty string?"
+            result.result = None
+            return result
+            
+        # is the numeric value safe?
+        result.err_msg = _safe_eval_check( result.result )
+        if result.err_msg != None:
+            result.result = None
+            return result
+
+        # let python do the math
+        try:
+            r = eval( result.result )
+            result.result = r
+        except Exception as e:
+            result.add_step("Exception: %s" % str(e) )
+            result.err_msg = "Syntax Error: %s" % str(e)
+            result.result = None
+
+        if result.err_msg != None:
+            result.result = None
+            
+        return result
+
+evaluator = ExpressionEvaluator( resolver )
 
--- tests/test_envmacros.py
+++ tests/test_envmacros.py
@@ -1,86 +1,266 @@
 from unittest import TestCase
 import sys
-
 import envmacros
 
+# change this to debug things
+VERBOSE=False
+
+def my_print( s ):
+    if VERBOSE:
+        print(s)
 
 def create_resolver():
-	resolver = envmacros.MacroResolver()
-	resolver.lookup.add('Foo','Bar')
-	resolver.lookup.add( 'child', 'Zack' )
-	resolver.lookup.add( 'parent_Zack', 'duane')
-	resolver.lookup.add( 'dog', 'Dolly')
-	resolver.lookup.add( 'A', '${B}')
-	resolver.lookup.add( 'B', '${C}')
-	resolver.lookup.add( 'C', '${D}')
-	resolver.lookup.add( 'D', '${A}')
-	return resolver
+    resolver = envmacros.MacroResolver()
+    resolver.lookup.add('Foo','Bar')
+    resolver.lookup.add( 'child', 'Zack' )
+    resolver.lookup.add( 'parent_Zack', 'duane')
+    resolver.lookup.add( 'dog', 'Dolly')
+    resolver.lookup.add( 'A', '${B}')
+    resolver.lookup.add( 'B', '${C}')
+    resolver.lookup.add( 'C', '${D}')
+    resolver.lookup.add( 'D', '${A}')
+    resolver.lookup.add( "seven", "7" )
+    resolver.lookup.add( "one", "1" )
+    resolver.lookup.add( "bad_two", "1+{$one}" )
+    resolver.lookup.add( "should_have_parens", "5-4" )
+    resolver.lookup.add( "good_two", "(1+${one})" )
+    resolver.lookup.add( "cos0", "cos(0)")
+    resolver.lookup.add( "cospi", "cos(3.14159)")
+    resolver.lookup.add( "four", "(2*${good_two})")
+    resolver.lookup.add( "ref_undef", "${undefined_thing}" )
+    resolver.lookup.add( "bad_macro", "${bad_macro" )
+    return resolver
+
+def create_eval():
+    r = create_resolver()
+    e = envmacros.ExpressionEvaluator( r )
+    return e
+
+def print_result( r ):
+    my_print("")
+    my_print("==========================")
+    s = r.result
+    if s == None:
+        t = "None"
+        s = "(none)"
+    else:
+        t = s.__class__.__name__
+    my_print(" result: (%s) '%s'" % (t,s) )
+    s = r.err_msg
+    if s == None:
+        s = "(none)"
+    my_print("err_msg: '%s'" % s )
+    for n,s in enumerate( r.steps ):
+        my_print( "%2d) %s" % (n,s))
+    my_print("==========================")
+    
 
 class MyTest( TestCase ):
+    def flush(self):
+        sys.stdout.flush()
+        sys.stderr.flush()
+    def setUp(self):
+        self.flush()
+        
+    def tearDown(self):
+        self.flush()
+                
+    def check_result( self, f, t ):
+        self.flush()
+        my_print("")
+        my_print("From: %s" % f)
+        my_print("  To: %s" % t)
+        resolver = create_resolver()
+                
+        r = resolver.resolve( f )
+        self.assertEqual( r.err_msg, None )
+        if t == r.result:
+            my_print("ok")
+            return
+        my_print("========================")
+        my_print("Expected: '%s'" % t)
+        my_print("========================")
+        print_result(result)
+        my_print("========================")
+        self.flush()
 
-	def setUp(self):
-		sys.stdout.flush()
-		sys.stderr.flush()
-		
-	def tearDown(self):
-		sys.stdout.flush()
-		sys.stderr.flush()
-	
-	def check_result( self, f, t ):
-		print("")
-		print("From: %s" % f)
-		print("  To: %s" % t)
-		resolver = create_resolver()
-		
-		r = resolver.resolve( f )
-		self.assertEqual( r.err_msg, None )
-		if t == r.result:
-			print("ok")
-			return
-		print("========================")
-		print("    From: '%s'" % f)
-		print("Expected: '%s'" % t)
-		print("  Actual: '%s'" % r.result)
-		print("------------------------")
-		for step in r.steps:
-			print(step)
-		self.fail('Wrong')
-		print("========================")
+    def test_010(self):
+        self.check_result( '', '' )     
+                
+    def test_020(self):
+        self.check_result(  'duane', 'duane' )
+                
+    def test_030(self):
+        self.check_result(  '${dog}', 'Dolly' )
+    def test_040(self):
+        self.check_result(  ' ${dog}', ' Dolly' )
+    def test_050(self):
+        self.check_result(  ' ${dog} ', ' Dolly ' )
+    def test_060(self):
+        self.check_result(  'abc->${dog}<-xyz' , 'abc->Dolly<-xyz' )
+    def test_070(self):
+        self.check_result(  'Double ${parent_${child}}', 'Double duane')
+                
+    def test_080(self):
+        # Missing closing brace
+        self.check_result( 'Borked ${parent_${child}', 'Borked ${parent_Zack' )
+            
+    def test_090(self):
+        r = create_resolver()
+        mr = r.resolve('${not_found}')
+        self.assertEqual( mr.result, None )
+        self.assertNotEqual( mr.err_msg, None )
+                
+    def test_100(self):
+        r = create_resolver()
+        result = envmacros.MacroResult()
+        result.pass_max = 15
+        mr = r.resolve( 'recursive ${A} for ever', result )
+        self.assertEqual( mr.result, None )
+        self.assertNotEqual( mr.err_msg, None )
+        print_result(mr)
 
-	def test_010(self):
-		self.check_result( '', '' )	
-		
-	def test_020(self):
-		self.check_result(  'duane', 'duane' )
-		
-	def test_030(self):
-		self.check_result(  '${dog}', 'Dolly' )
-	def test_040(self):
-		self.check_result(  ' ${dog}', ' Dolly' )
-	def test_050(self):
-		self.check_result(  ' ${dog} ', ' Dolly ' )
-	def test_060(self):
-		self.check_result(  'abc->${dog}<-xyz' , 'abc->Dolly<-xyz' )
-	def test_070(self):
-		self.check_result(  'Double ${parent_${child}}', 'Double duane')
-		
-	def test_080(self):
-		# Missing closing brace
-		self.check_result( 'Borked ${parent_${child}', 'Borked ${parent_Zack' )
-		
-	def test_090(self):
-		r = create_resolver()
-		mr = r.resolve('${not_found}')
-		self.assertEqual( mr.result, None )
-		self.assertNotEqual( mr.err_msg, None )
-		
-	def test_100(self):
-		r = create_resolver()
-		r.pass_max = 15
-		mr = r.resolve( 'recursive ${A} for ever' )
-		self.assertEqual( mr.result, None )
-		self.assertNotEqual( mr.err_msg, None )
-		print(mr.result)
-		for step in mr.steps:
-			print(step)
-		
+    def eval_xx( self, str_expr ):
+        self.flush()
+        my_print("EVAL: %s" % str_expr)
+        r = create_eval()
+        result = r.eval( str_expr )
+        print_result(result)
+        my_print("Check result")
+        return result
+        
+    def eval_bad( self, str_expr ):
+        result = self.eval_xx( str_expr )
+        bad = False
+        if result.result == None:
+            my_print("GOOD: result is none")
+        else:
+            my_print("WRONG: result should be None, it is: '%s'" % result.result)
+            bad =True
+        if result.err_msg == None:
+            my_print("WRONG: err_msg is None, it should have value")
+            bad = True
+        else:
+            my_print("GOOD: err_msg = %s" % result.err_msg )
+        if bad:
+            self.fail()
+        else:
+            # self.success()
+            pass
+
+    def eval_good( self, expect, str_expr):
+        self.flush()
+        result = self.eval_xx( str_expr )
+        my_print("============================")
+        my_print("Expected: %d" % expect )
+        my_print("============================")
+        bad = False
+        if result.err_msg == None:
+            my_print("Good: err_msg is None")
+        else:
+            my_print("WRONG: err_msg is not None, it is: '%s'" % result.err_msg )
+            bad =True
+        my_print("errmsg ok")
+        v = 0
+        if result.result is None:
+            bad =True
+            my_print("WRONG: result is none!")
+        else:
+            v = result.result
+            my_print("GOOD: Result is not none, it is '%s'" % v )
+        if not bad:
+            if isinstance( v, int ):
+                my_print("GOOD: Result is type int()")
+            else:
+                if isinstance(v,float) and (v == 1.0):
+                    # Then this is good
+                    my_print("OK 1.0 is a float")
+                else:
+                    bad = True
+                    my_print("WRONG: result is not type int it is: %s" % v.__class__.__name__ )
+
+        if not bad:
+            if expect == v:
+                return
+            bad = True
+            my_print("WRONG: Expect %s" % expect )
+            my_print("WRONG:    got %s" % v ) 
+        self.flush()
+        if bad:
+            self.fail()
+        else:
+            # good
+            pass
+    def test_200(self):
+        self.eval_bad( "" )
+
+    def test_210(self):
+        self.eval_bad( "dog")
+                         
+    def test_210(self):
+        self.eval_bad( "3 * - / 4" )
+
+    def test_210(self):
+        self.eval_bad( "${not_defined_macro}" )
+
+    def test_210(self):
+        self.eval_good( 1, "1" )
+
+    def test_210(self):
+        self.eval_good( 4, "1 + 1 + 1 + 1" )
+        
+    def test_210(self):
+        self.eval_good( 4, "1 + 1 + ${one} + 1" )
+        
+
+    def test_215( self ):
+        self.eval_good( 4, "2 * ${good_two}")
+
+    def test_216( self ):
+        self.eval_bad( "2 * ${bad_two}")
+        
+    def test_217( self ):
+        # this is 2 * 5-4, or (2*5)-4, which is 6
+        self.eval_good( 6, "2 * ${should_have_parens}")
+
+    def test_220( self ):
+        self.eval_good( 1, "2 / (${good_two})")
+
+    def test_230( self ):
+        self.eval_good( True, "True")
+        
+    def test_231( self ):
+        self.eval_good( True, "False!=True")
+    def test_232( self ):
+        self.eval_good( True, "False !=  True")
+    def test_233( self ):
+        self.eval_good( True, "  False !=  True  ")
+                         
+
+    def test_240( self ):
+        r = create_eval()
+        mr = r.eval("cos(0)")
+        self.assertTrue( mr.result != None ) 
+        self.assertTrue( mr.err_msg == None )
+        self.assertTrue( mr.result, 1 )
+
+    def test_250(self):
+        self.eval_good(    1,   "${one}" )
+    def test_251(self):
+        self.eval_good( 0x0100,  "${one}<<(2*${four})")
+    def test_252(self):
+        self.eval_good( 0x0100,  "((0x0100 & (${one}<<(2*${four}))))")
+    def test_253(self):
+        self.eval_good( True,   "((0x0100 & (${one}<<(2*${four}))) != 0)")
+    def test_254(self):
+        self.eval_good( True,   "((0x0100 & (${one}<<(2*${four}))) != 0) == True"  )
+
+    def test_255(self):
+        self.eval_good( True,   "True and True" )
+
+    def test_256(self):
+        self.eval_good( True,   "True and not False")
+
+    def test_257(self):
+        # Like: C's (!!x)
+        self.eval_good( 1,   "not not 7")
